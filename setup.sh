378e4d9d1a570f13f2c3b3b6c48623e69e43f9f7
soramugi <soramugi.chika@gmail.com>
2012-04-05 13:37:15 +0900

    if de shinborikku rinku attara tukuranai

--- setup.sh
+++ setup.sh
@@ -17,7 +17,10 @@ git config --global alias.br 'branch'
 DOT_FILES=( .bashrc .vimrc .gvimrc .vim .screenrc )
 for file in ${DOT_FILES[@]}
 do
+if [ ! -e $HOME/$file ]
+then
     ln -s $HOME/dotfiles/$file $HOME/$file
+fi
 done
 
 #local環境設定ファイル
