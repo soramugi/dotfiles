16e20f45e3b4f18d0de027c0dd6f8030da248ae9
soramugi <soramugi.chika@gmail.com>
2012-04-05 12:45:35 +0900

    gitエイリアス変更diをdfに

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ git config --global core.editor 'vim -c "set fenc=utf-8"'
 git config --global alias.co 'checkout'
 git config --global alias.st 'status'
 git config --global alias.ci 'commit'
-git config --global alias.di 'diff'
+git config --global alias.df 'diff'
 git config --global alias.br 'branch'
 
 #シンボリックりんく
