85f0dc64eac918faf89172419816d8b4c03d06d5
soramugi <soramugi.chika@gmail.com>
2012-04-18 19:16:33 +0900

    vim最新版をホームディレクトリに追加するバッチおためし

---
+++ newvim.sh
@@ -0,0 +1,15 @@
+mkdir -p $HOME/local/{bin, src}
+cd $HOME/local/src/
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
+tar jxvf vim-7.3.tar.bz2
+cd vim73
+
+./configure \
+--enable-multibyte \
+--enable-xim \
+--enable-fontset \
+--with-features=big \
+--prefix=$HOME/local
+
+make
+make install
