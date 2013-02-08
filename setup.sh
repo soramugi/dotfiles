a3b69619060baa5a93c2b7649c6216705fe43af7
cube <cube@sonicmoov.com>
2013-02-08 12:37:11 +0900

    neobundle install コマンドを外部ファイル化

--- setup.sh
+++ setup.sh
@@ -5,9 +5,6 @@ if [ -e $HOME/dotfiles/gitSetup.sh ] ; then
   source $HOME/dotfiles/gitSetup.sh
 fi
 
-# neobundle install
-vim -N -u NONE -i NONE -V1 -e -s --cmd "source ~/dotfiles/.vimrc" --cmd NeoBundleInstall! --cmd qall!
-
 # bashrcの書き込み
 if [ ! -e $HOME/.bash_aliases ] && [ ! -e $HOME/.profile ] ; then
   echo 'if [ -f ~/.bash_aliases ] ; then' >> $HOME/.bashrc
