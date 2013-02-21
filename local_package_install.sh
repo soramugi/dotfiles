e01d9f73eeb3d18e03e4da4e8b7227de1b0f01e3
cube <cube@sonicmoov.com>
2013-02-21 17:09:16 +0900

    package install debian,redhat

--- local_package_install.sh
+++ local_package_install.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 # debian
-#sudo apt-get install mercurial libncurses-dev make gcc wget
+if which apt-get 2>&1 >/dev/null; then
+  sudo apt-get install mercurial libncurses-dev make gcc wget
+fi
 
 # redhat
-#sudo yum install mercurial ncurses-devel make gcc wget
+if which yum 2>&1 >/dev/null; then
+  sudo yum install mercurial ncurses-devel make gcc wget
+fi
 
 mkdir -p $HOME/local/bin
 mkdir -p $HOME/local/src
@@ -15,9 +19,9 @@ cd $HOME/local/src/
 hg clone https://vim.googlecode.com/hg/ vim
 cd vim
 ./configure \
---prefix=$HOME/local \
---enable-multibyte \
---disable-selinux
+  --prefix=$HOME/local \
+  --enable-multibyte \
+  --disable-selinux
 
 make
 sudo make install
@@ -41,7 +45,7 @@ tar zxvf tmux-1.6.tar.gz
 
 cd $HOME/local/src/tmux-1.6
 ./configure --prefix=$HOME/local \
-CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" \
-LDFLAGS="-static -L$HOME/local/include -L$HOME/local/ncurses -L$HOME/local/lib"
+  CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" \
+  LDFLAGS="-static -L$HOME/local/include -L$HOME/local/ncurses -L$HOME/local/lib"
 make
 sudo make install
