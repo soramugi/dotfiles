daca26ec106837c87f0e4ce5a52c655d38d90f52
soramugi <soramugi.chika@gmail.com>
2014-11-15 22:04:02 +0900

    add global tool to ruby test

---
+++ guard.rb
@@ -0,0 +1,67 @@
+if Gem::Specification.find_all_by_name('guard-bundler').any?
+  guard 'bundler' do
+    watch('Gemfile')
+  end
+end
+
+if Gem::Specification.find_all_by_name('guard-rspec').any?
+  zeus = File.exists?('.zeus.sock')
+  spork = Gem::Specification.find_all_by_name('spork').any?
+  spring = Gem::Specification.find_all_by_name('spring').any?
+
+  cmd = ''
+  cmd += 'zeus ' if zeus
+  cmd += 'spring ' if spring
+  cmd += 'rspec --color --fail-fast --profile 5 --format documentation '
+  cmd += '--drb ' if spork
+
+  guard 'rspec', :all_on_start => false, :all_after_pass => false, :cmd => cmd do
+    # Spec files
+    watch(%r{^spec/.+_spec\.rb$})
+
+    # Factories
+    watch(%r{^spec/factories/(.+)\.rb$})               { |m| ["spec/models/#{m[1].singularize}_spec.rb", "spec/controllers/#{m[1]}_controller_spec.rb", "spec/requests/#{m[1]}_spec.rb"] }
+
+    # Rails specific
+    watch(%r{^app/(.+)\.rb$})                          { |m| "spec/#{m[1]}_spec.rb" }
+    watch(%r{^lib/(.+)\.rb$})                          { |m| "spec/lib/#{m[1]}_spec.rb" }
+    watch(%r{^app/controllers/(.+)_(controller)\.rb$}) { |m| ["spec/#{m[2]}s/#{m[1]}_#{m[2]}_spec.rb", "spec/acceptance/#{m[1]}_spec.rb"] }
+    watch('app/controllers/application_controller.rb') { "spec/controllers" }
+    watch(%r{^app/views/(.+)$})                        { |m| "spec/views/#{m[1]}_spec.rb" }
+
+    # Capybara request specs
+    watch(%r{^app/views/(.+)/.*\.(erb|haml|slim)$})    { |m| "spec/requests/#{m[1]}_spec.rb" }
+
+    # Controller specs for views
+    watch(%r{^app/views/(.+)/.*\.(erb|haml|slim)$})    { |m| "spec/controllers/#{m[1]}_spec.rb" }
+
+    # Turnip features and steps
+    watch(%r{^spec/acceptance/(.+)\.feature$})
+    watch(%r{^spec/acceptance/steps/(.+)_steps\.rb$})  { |m| Dir[File.join("**/#{m[1]}.feature")][0] || 'spec/acceptance' }
+
+    # Runs all specs when something in /lib is modified (for Ruby gems development)
+    if Dir.glob('*.gemspec').any?
+      watch(%r{^lib/.+\.rb$}) { "spec" }
+    end
+
+    # Standalone projects
+    watch(%r{^(.+)\.rb$}) { |m| "spec/#{m[1]}_spec.rb" }
+
+    # Ignore Zeus
+    ignore(/\.zeus\.sock/)
+  end
+end
+
+if Gem::Specification.find_all_by_name('guard-test').any?
+  guard 'test', :all_on_start => false, :all_after_pass => false, :bundler => true, :keep_failed => false, :cli => '--verbose=normal', :drb => true do
+    watch(%r{^lib/(.+)\.rb$})     { |m| "test/#{m[1]}_test.rb" }
+    watch(%r{^test/.+_test\.rb$})
+    watch('test/test_helper.rb')  { "test" }
+
+    # Rails
+    watch(%r{^app/models/(.+)\.rb$})                   { |m| "test/unit/#{m[1]}_test.rb" }
+    watch(%r{^app/controllers/(.+)\.rb$})              { |m| "test/functional/#{m[1]}_test.rb" }
+    watch(%r{^app/views/.+\.rb$})                      { "test/integration" }
+    watch('app/controllers/application_controller.rb') { ["test/functional", "test/integration"] }
+  end
+end
--- vim/filetype.vim
+++ vim/filetype.vim
@@ -1,13 +1,14 @@
 "ruby"
-autocmd BufRead,BufNewFile Gemfile     setfiletype ruby
-autocmd BufRead,BufNewFile Guardfile   setfiletype ruby
-autocmd BufRead,BufNewFile Vagrantfile setfiletype ruby
-autocmd BufRead,BufNewFile Berksfile   setfiletype ruby
-autocmd BufRead,BufNewFile Capfile     setfiletype ruby
-autocmd BufRead,BufNewFile *.cap      setfiletype ruby
-autocmd BufRead,BufNewFile *.rabl      setfiletype ruby
-autocmd BufRead,BufNewFile *.jbuilder  setfiletype ruby
-autocmd BufRead,BufNewFile *.ru        setfiletype ruby
+autocmd BufRead,BufNewFile Gemfile       setfiletype ruby
+autocmd BufRead,BufNewFile Guardfile     setfiletype ruby
+autocmd BufRead,BufNewFile Vagrantfile   setfiletype ruby
+autocmd BufRead,BufNewFile Berksfile     setfiletype ruby
+autocmd BufRead,BufNewFile Capfile       setfiletype ruby
+autocmd BufRead,BufNewFile *.cap         setfiletype ruby
+autocmd BufRead,BufNewFile *.rabl        setfiletype ruby
+autocmd BufRead,BufNewFile *.jbuilder    setfiletype ruby
+autocmd BufRead,BufNewFile *.ru          setfiletype ruby
+autocmd BufRead,BufNewFile simplecov     setfiletype ruby
 autocmd BufWinEnter,BufNewFile *_spec.rb setfiletype ruby.rspec
 autocmd BufWinEnter,BufNewFile *_test.rb setfiletype ruby.unit
 
